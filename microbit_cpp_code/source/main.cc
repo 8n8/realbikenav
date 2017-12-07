92a05b49a2c31162d461a811f6ff0fe374418ad1
5-o <>
2017-12-07 23:23:03 +0000

    Written the first draft of the microbit c++ code and got it through the compiler.  Don't know if it works yet.

--- microbit_cpp_code/source/main.cc
+++ microbit_cpp_code/source/main.cc
@@ -2,11 +2,24 @@
 
 MicroBit uBit;
 
+struct leds {
+    bool new_destination;
+    int direction;
+    int recording_state;
+    bool error_state;
+};
+
+struct accel {
+    int x;
+    int y;
+    int z;
+};
+
 // It represents the data for sending to the output
-struct OutputSlashState {
-    bool[16] display;
+struct State {
+    leds display;
     int compass;
-    int[3] acceleration;
+    accel acceleration;
     int buttonAcounter;
     int buttonBcounter;
 };
@@ -24,30 +37,67 @@ struct OutputSlashState {
 // It represents all the input readings after parsing.
 struct InputData {
     bool new_input;
-    bool[16] display;
-    int direction;
-    int[3] acceleration;
+    leds display;
+    int compass;
+    accel acceleration;
     bool buttonA;
     bool buttonB;
 };
 
-bool[16] parse_display(char raw_display) {
-    int direction = (raw_display >> 1) & 15;
+
+// It converts the display byte received from the serial port into
+// the led struct.
+void parse_display(const char &raw_display, leds &display) {
+    display.new_destination = raw_display & 1;
+    display.direction = (raw_display >> 1) & 15;
+    display.recording_state = (raw_display >> 5) & 3;
+    display.error_state = (raw_display >> 7) & 1;
 }
 
 // It reads all the inputs.
 void read_input(InputData &input_data) {
     input_data.new_input = uBit.serial.readable();
-    input_data.display = parse_display(uBit.serial.read(uBit.ASYNC));
+    parse_display(uBit.serial.read(1, ASYNC).charAt(0), input_data.display);
     input_data.compass = uBit.compass.heading();
-    input_data.acceleration[0] = uBit.accelerometer.getX();
-    input_data.acceleration[1] = uBit.accelerometer.getY();
-    input_data.acceleration[2] = uBit.accelerometer.getZ();
-    input_data.buttonA = uBit.buttonA.wasPressed();
-    input_data.buttonB = uBit.buttonB.wasPressed();
+    input_data.acceleration.x = uBit.accelerometer.getX();
+    input_data.acceleration.y = uBit.accelerometer.getY();
+    input_data.acceleration.z = uBit.accelerometer.getZ();
+    input_data.buttonA = uBit.buttonA.isPressed();
+    input_data.buttonB = uBit.buttonB.isPressed();
 }
     
 
+void update_state(State &state, const InputData &input_data) {
+    if (input_data.new_input) {
+        state.display = input_data.display;
+    }
+    state.compass = input_data.compass;
+    state.acceleration = input_data.acceleration;
+    state.buttonAcounter = 
+        (state.buttonAcounter < 10000) * 
+        (state.buttonAcounter + input_data.buttonA);
+    state.buttonBcounter =
+        (state.buttonBcounter < 10000) *
+        (state.buttonBcounter + input_data.buttonB);
+}
+
+
+void sendInt(const int &the_int) {
+    uBit.serial.sendChar(the_int & 0xff);
+    uBit.serial.sendChar((the_int >> 8) & 0xff);
+    uBit.serial.sendChar((the_int >> 16) & 0xff);
+    uBit.serial.sendChar((the_int >> 24) & 0xff);
+}
+
+
+void send_output(const State &state) {
+    sendInt(state.compass);
+    sendInt(state.acceleration.x);
+    sendInt(state.acceleration.y);
+    sendInt(state.acceleration.z);
+    sendInt(state.buttonAcounter);
+    sendInt(state.buttonBcounter);
+}
 
 
 int main() {
@@ -57,8 +107,8 @@ int main() {
     // Insert your code here!
     uBit.display.scroll("HELLO WORLD! :)");
 
-    StateSlashOutput state;
-    state.display = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    State state;
+    state.display = {false, 0, 0, false};
     state.compass = 0;
     state.acceleration = {0, 0, 0};
     state.buttonAcounter = 0;
@@ -66,10 +116,11 @@ int main() {
 
     InputData input_data;
 
-    while(True) {
+    while(true) {
         send_output(state);
         read_input(input_data);
-        update_state(state, raw_input_data)
+        update_state(state, input_data);
+    }
 
     // If main exits, there may still be other fibers running or
     // registered event handlers etc.
@@ -78,4 +129,3 @@ int main() {
     // efficient sleep.
     release_fiber();
 }
-
